f5e02de6ee1ea8d93a7950026e78a2accd031230
Lin-Hi <1318538311@qq.com>
2022-11-29 16:38:04 +0800

    成功运行视频组件2.0

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,46 +1,55 @@
 <template>
-  <div style="height: 20px;margin:0 0 50px 0;">
+  <div>
     <span style="font-size: 20px;margin:0 100px 0 0">章节1：HelloWorld</span>
     <span style="font-size: 15px;">by 张三</span>
     <el-button size="default" style="margin:0 0 0 20px;width: 130px;vertical-align: middle;">订阅</el-button>
     <br>
-    <div style="height: 20px;margin-top: 10px;color: gray">
-      <el-icon size="15px" style="margin: 0 0 0 20px;vertical-align: middle">
+    <div style="height: 20px;margin-top:0;color: gray">
+      <el-icon size="14px" style="margin: 0 0 0 20px;vertical-align: middle">
         <Warning/>
       </el-icon>
-      未经作者授权禁止转载
+      <span style="font-size: 13px"> 未经作者授权禁止转载</span>
     </div>
   </div>
 
-  <video-player
-      class="video-player vjs-big-play-centered"
-      src="https://vjs.zencdn.net/v/oceans.mp4"
-      poster="https://vjs.zencdn.net/v/oceans.png"
-      crossorigin="anonymous"
-      playsinline
-      controls
-      :volume="0.6"
-      :height="320"
-      :playback-rates="[0.7, 1.0, 1.5, 2.0]"
-      @mounted="handleMounted"
-      @ready="handleEvent($event)"
-      @play="handleEvent($event)"
-      @pause="handleEvent($event)"
-      @ended="handleEvent($event)"
-      @loadeddata="handleEvent($event)"
-      @waiting="handleEvent($event)"
-      @playing="handleEvent($event)"
-      @canplay="handleEvent($event)"
-      @canplaythrough="handleEvent($event)"
-      @timeupdate="handleEvent(player?.currentTime())"
-  />
+  <el-row style="margin:20px 0 0 0;height: 350px" :gutter="30">
+    <el-col :span="16">
+      <video-player
+          class="video-player vjs-big-play-centered"
+          src="https://vjs.zencdn.net/v/oceans.mp4"
+          poster="https://vjs.zencdn.net/v/oceans.png"
+          crossorigin="anonymous"
+          playsinline
+          controls
+          :volume="0.6"
+          :playback-rates="[0.7, 1.0, 1.5, 2.0]"
+          @mounted="handleMounted"
+          @ready="handleEvent($event)"
+          @play="handleEvent($event)"
+          @pause="handleEvent($event)"
+          @ended="handleEvent($event)"
+          @loadeddata="handleEvent($event)"
+          @waiting="handleEvent($event)"
+          @playing="handleEvent($event)"
+          @canplay="handleEvent($event)"
+          @canplaythrough="handleEvent($event)"
+          @timeupdate="handleEvent(player?.currentTime())"
+          style="height: 100%"
+      />
+    </el-col>
+    <el-col :span="8">
+      <div class="grid-content ep-bg-purple" style="background-color: lightblue;height:100%;"/>
+    </el-col>
+  </el-row>
+
+
 
 </template>
 
 <script lang="ts">
-import { defineComponent, shallowRef } from 'vue'
-import {VideoJsPlayer}  from 'video.js'
-import { VideoPlayer } from '@videojs-player/vue'
+import {defineComponent, shallowRef} from 'vue'
+import {VideoJsPlayer} from 'video.js'
+import {VideoPlayer} from '@videojs-player/vue'
 import 'video.js/dist/video-js.css'
 
 export default defineComponent({
@@ -61,7 +70,7 @@ export default defineComponent({
       console.log('Basic player event', log)
     }
 
-    return { player, handleMounted, handleEvent }
+    return {player, handleMounted, handleEvent}
   }
 })
 </script>
