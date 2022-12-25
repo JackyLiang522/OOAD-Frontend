65a407ad26b17592ed5c5cbebae2dae2a06c97ae
Lin-Hi <1318538311@qq.com>
2022-12-25 19:53:17 +0800

    /teacher/detail/content Grade 写完

--- src/views/CourseOnline/TeacherCenter/Detail/Content/Grade.vue
+++ src/views/CourseOnline/TeacherCenter/Detail/Content/Grade.vue
@@ -1,13 +1,95 @@
 <template>
-  <h4 style="margin: 0 0 0 0">{{ `第${chapterInfo.number}章` }}</h4>
-  <p>{{ chapterInfo.title }}</p>
-  <p style="color: gray;font-size: 13px">{{ chapterInfo.intro }}</p>
+  <h4 style="margin: 0 0 0 10px">{{ `第${chapterInfo.number}章 ${chapterInfo.title}` }}</h4>
+
+  <div style="display: flex;justify-content: center">
+    <el-table
+        :data="table_data"
+        border
+        style="width: 550px;margin:20px 20px 0 20px;"
+        height="250px"
+        stripe
+        :header-cell-style="{'text-align':'center'}"
+        :cell-style="{'text-align':'center'}"
+    >
+      <el-table-column prop="name" label="学生姓名" width="200px"/>
+      <el-table-column prop="score" label="分数" width="350px"/>
+    </el-table>
+  </div>
+
+  <div style="display: flex;justify-content: center;margin-top: 20px">
+    <el-button type="primary" plain>
+      导出为
+    </el-button>
+  </div>
+
 </template>
 
 <script>
+import {reactive} from "vue";
+
 export default {
   name: "Grade",
   props: ['chapterInfo'],
+  setup() {
+    const table_data = reactive([
+      {
+        name: 'Stu 1',
+        score: 10,
+      }, {
+        name: 'stu 2',
+        score: 20,
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }, {
+        name: 'stu 3',
+        score: 30
+      }
+    ])
+
+    return {
+      table_data
+    }
+  }
 }
 </script>
 
--- src/views/CourseOnline/TeacherCenter/Detail/Content/Quiz.vue
+++ src/views/CourseOnline/TeacherCenter/Detail/Content/Quiz.vue
@@ -1,7 +1,5 @@
 <template>
-  <h4 style="margin: 0 0 0 0">{{ `第${chapterInfo.number}章` }}</h4>
-  <p>{{ chapterInfo.title }}</p>
-  <p style="color: gray;font-size: 13px">{{ chapterInfo.intro }}</p>
+  <h4 style="margin: 0 0 0 10px">{{ `第${chapterInfo.number}章 ${chapterInfo.title}` }}</h4>
 </template>
 
 <script>
