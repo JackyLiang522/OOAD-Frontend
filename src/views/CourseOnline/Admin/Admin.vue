2900b6ac583f589b037a5c64d846e482087758d0
源 <73240868+JustinLiang522@users.noreply.github.com>
2022-12-26 19:31:02 +0800

    完成管理员审核交互

--- src/views/CourseOnline/Admin/Admin.vue
+++ src/views/CourseOnline/Admin/Admin.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="display: flex;justify-content: center">
-    <el-table
+    <!-- <el-table
         :data="table_data"
         border
         style="width: 700px;margin:20px 20px 0 20px;"
@@ -8,8 +8,8 @@
         :header-cell-style="{'text-align':'center'}"
         :cell-style="{'text-align':'center'}"
     >
-      <el-table-column prop="name" label="课程名字" width="100px"/>
-      <el-table-column prop="intro" label="课程简介" width="350px"/>
+      <el-table-column prop="courseName" label="课程名字" width="100px"/>
+      <el-table-column prop="introduction" label="课程简介" width="350px"/>
       <el-table-column prop="price" label="课程价格" width="100px"/>
       <el-table-column label="审核" width="150px">
         <el-button
@@ -23,58 +23,74 @@
           拒绝
         </el-button>
       </el-table-column>
-    </el-table>
+    </el-table> -->
+    
+    <table border="1" style="width: 700px;margin:20px 20px 0 20px;">
+    <thead>
+      <tr>
+        <th style="text-align:center">课程名字</th>
+        <th style="text-align:center">课程简介</th>
+        <th style="text-align:center">课程价格</th>
+        <th style="text-align:center">审核</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in table_data" :key="row.courseName">
+        <td style="text-align:center">{{ row.courseName }}</td>
+        <td style="text-align:center">{{ row.introduction }}</td>
+        <td style="text-align:center">{{ row.price }}</td>
+        <td style="text-align:center">
+          <el-button type="success" size="small" round @click="handlePass(row)">通过</el-button>
+          <el-button type="danger" size="small" round @click="handleReject(row)">拒绝</el-button>
+        </td>
+      </tr>
+    </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import {reactive} from "vue";
+import axios from "axios";
+import store from "@/store";
+import {ElMessage} from 'element-plus'
 
 export default {
   name: "Admin",
-  setup() {
-    const table_data = reactive([
-      {
-        name: 'Java',
-        intro: 'This is hava not jvav.',
-        price: 1.11,
-        cover: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
-      }, {
-        name: 'Java',
-        intro: 'This is hava not jvav.',
-        price: 1.11,
-        cover: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
-      }, {
-        name: 'Java',
-        intro: 'This is hava not jvav.',
-        price: 1.11,
-        cover: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
-      }, {
-        name: 'Java',
-        intro: 'This is hava not jvav.',
-        price: 1.11,
-        cover: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
-      }, {
-        name: 'Java',
-        intro: 'This is hava not jvav.',
-        price: 1.11,
-        cover: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
-      }, {
-        name: 'Java',
-        intro: 'This is hava not jvav.',
-        price: 1.11,
-        cover: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
-      }, {
-        name: 'Java',
-        intro: 'This is hava not jvav.',
-        price: 1.11,
-        cover: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
-      }
-    ])
-
+  data() {
     return {
-      table_data
+      table_data: []
     }
+  },
+  methods: {
+    handlePass(row) {
+      axios.post(`http://${store.state.host}/api/course/update_status?courseId=${row.id}&status=1`).then((response) => {
+        ElMessage.success({
+          message: '操作成功',
+          type: 'success',
+          duration: 2000,
+        })
+      })
+      axios.get(`http://${store.state.host}/api/course/list_under_review`).then((response) => {
+        this.table_data = response.data
+      })
+    },
+    handleReject(row) {
+      axios.post(`http://${store.state.host}/api/course/update_status?courseId=${row.id}&status=2`).then((response) => {
+        ElMessage.success({
+          message: '操作成功',
+          type: 'success',
+          duration: 2000,
+        })
+      })
+      axios.get(`http://${store.state.host}/api/course/list_under_review`).then((response) => {
+        this.table_data = response.data
+      })
+    }
+  },
+  async created() {
+    axios.get(`http://${store.state.host}/api/course/list_under_review`).then((response) => {
+      this.table_data = response.data
+    })
   }
 }
 </script>
