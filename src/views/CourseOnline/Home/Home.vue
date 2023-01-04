a34d801d3636e51e4b47de9a991e12cfcc359bc8
Lin-Hi <1318538311@qq.com>
2023-01-04 18:10:23 +0800

    修改首页分页

--- src/views/CourseOnline/Home/Home.vue
+++ src/views/CourseOnline/Home/Home.vue
@@ -51,7 +51,7 @@ export default {
 
     function changeShown() {
       const temp = []
-      for (let i = (currentPage - 1) * pageSize + 1; i < pageSize; i++) {
+      for (let i = (currentPage.value - 1) * pageSize.value + 1; i < pageSize.value; i++) {
         temp.push(courses[i])
       }
       shownCourses.value = temp
